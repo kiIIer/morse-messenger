3540ce86cc7bc8034685eb5c688fb737385537c6
killer <michael.molchanov.2004@gmail.com>
2023-02-02 14:35:37 +0100

    test server

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,5 +1,35 @@
-use morse_messenger as morser;
+use morser::{
+    messenger_server::{Messenger, MessengerServer},
+    Signal,
+};
+use tonic::{transport::Server, Request, Response, Status};
 
-fn main() {
-    morser::say_hi("server");
+pub mod morser {
+    tonic::include_proto!("morser");
+}
+
+#[derive(Debug, Default)]
+pub struct MorserService {}
+
+#[tonic::async_trait]
+impl Messenger for MorserService {
+    async fn chat(&self, request: Request<Signal>) -> Result<Response<Signal>, Status> {
+        let r = request.into_inner();
+        let state = r.state;
+        println!("{}", state);
+        Ok(Response::new(morser::Signal { state }))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let address = "[::1]:8080".parse().unwrap();
+    let morser_service = MorserService::default();
+
+    Server::builder()
+        .add_service(MessengerServer::new(morser_service))
+        .serve(address)
+        .await?;
+
+    Ok(())
 }
