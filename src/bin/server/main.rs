71edb0093190b4c528d0d729039f1847a19cd944
killer <michael.molchanov.2004@gmail.com>
2023-02-03 19:05:52 +0100

    EEEEEEEEEEEEEEEE BOIIIIIIIII The server works as broadcaster

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -2,14 +2,14 @@ use crate::morser::Signal;
 use futures_core::Stream;
 use morser::messenger_server;
 use std::error::Error;
-use std::io::ErrorKind;
 use std::net::ToSocketAddrs;
 use std::pin::Pin;
-use tokio::sync::mpsc;
-use tokio_stream::wrappers::ReceiverStream;
+use tokio::sync::broadcast;
 use tokio_stream::StreamExt;
 use tonic::transport::Server;
 use tonic::{Request, Response, Status, Streaming};
+
+type Message = Result<Signal, Status>;
 
 pub mod morser {
     tonic::include_proto!("morser");
@@ -38,11 +38,19 @@ fn match_for_io_error(err_status: &Status) -> Option<&std::io::Error> {
     }
 }
 
-#[derive(Default)]
-struct MessengerService {}
+pub struct MessengerServer {
+    tx: broadcast::Sender<Message>,
+}
+
+impl Default for MessengerServer {
+    fn default() -> Self {
+        let (tx, _) = broadcast::channel(16);
+        MessengerServer { tx }
+    }
+}
 
 #[tonic::async_trait]
-impl messenger_server::Messenger for MessengerService {
+impl messenger_server::Messenger for MessengerServer {
     type ChatStream = Pin<Box<dyn Stream<Item = Result<Signal, Status>> + Send>>;
 
     async fn chat(
@@ -51,51 +59,47 @@ impl messenger_server::Messenger for MessengerService {
     ) -> Result<Response<Self::ChatStream>, Status> {
         let mut in_stream = request.into_inner();
 
-        let (tx, rx) = mpsc::channel(128);
-
+        let tx = self.tx.clone();
+        let rx = tx.subscribe();
         tokio::spawn(async move {
-            while let Some(result) = in_stream.next().await {
-                match result {
-                    Ok(v) => {
-                        println!("Got {}", v.state);
-                        tx.send(Ok(Signal { state: v.state }))
-                            .await
-                            .expect("working rx")
-                    }
-                    Err(err) => {
-                        if let Some(io_err) = match_for_io_error(&err) {
-                            if io_err.kind() == ErrorKind::BrokenPipe {
-                                // here you can handle special case when client
-                                // disconnected in unexpected way
-                                eprintln!("\tclient disconnected: broken pipe");
-                                break;
-                            }
-                        }
+            loop {
+                tokio::select! {
+                                read = in_stream.next() => {
+                                    if let Some(result) = read{
+                                        match result{
+                                            Ok(v) => {
+                                                tx.send(Ok(v)).expect("Couldn't send");
+                                            },
+                                            Err(err) => {
+                                                if let Some(io_err) = match_for_io_error(&err){
+                                                    eprintln!("{:?}", io_err);
+                                                    eprintln!("\tclient disconnected: broken pipe");
+                                                }
 
-                        match tx.send(Err(err)).await {
-                            Ok(_) => (),
-                            Err(_err) => break,
-                        }
-                    }
+                                                match tx.send(Err(err)){
+                                                    Ok(_) => (),
+                                                    Err(_) => break,
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                 }
             }
             println!("\tstream ended");
         });
-
-        let out_stream = ReceiverStream::new(rx);
-
+        let out_stream = tokio_stream::wrappers::BroadcastStream::from(rx).map(|x| x.unwrap());
         Ok(Response::new(Box::pin(out_stream) as Self::ChatStream))
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let service = MessengerService::default();
+async fn main() -> Result<(), Box<dyn Error>> {
+    let server = MessengerServer::default();
     Server::builder()
-        .add_service(morser::messenger_server::MessengerServer::new(service))
+        .add_service(morser::messenger_server::MessengerServer::new(server))
         .serve("[::1]:50051".to_socket_addrs().unwrap().next().unwrap())
         .await
         .unwrap();
-
     Ok(())
 }
