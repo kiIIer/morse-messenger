88b747bca3645d021087221f5236534a06d384ad
killer <michael.molchanov.2004@gmail.com>
2023-02-06 16:20:19 +0100

    added TODO so I dont forget to fix docs

--- src/client/app/components/home.rs
+++ src/client/app/components/home.rs
@@ -1,14 +1,16 @@
 pub struct HomeComponent {
     heading: String,
-    tutorial: String
+    tutorial: String,
 }
 
 impl Default for HomeComponent {
     fn default() -> Self {
-        let tutorial = String::from("Well this is supposed to be a quick tutorial over how the app works.");
-        HomeComponent{
+        // TODO: Write proper doc
+        let tutorial =
+            String::from("Well this is supposed to be a quick tutorial over how the app works.");
+        HomeComponent {
             heading: String::from("Morse Code Messenger"),
-            tutorial
+            tutorial,
         }
     }
 }
