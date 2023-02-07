be2e0cc975878315d88b51d8742f9b5b064766a1
killer <michael.molchanov.2004@gmail.com>
2023-02-07 15:01:00 +0100

    tabs enum

--- src/client/app/components/tabs.rs
+++ src/client/app/components/tabs.rs
@@ -2,6 +2,24 @@ use tui::backend::Backend;
 use tui::layout::Rect;
 use tui::Frame;
 
+pub enum MenuItem {
+    Home,
+    Signal,
+    Cheat,
+    Trans,
+}
+
+impl From<MenuItem> for usize {
+    fn from(value: MenuItem) -> Self {
+        match value {
+            MenuItem::Home => 0,
+            MenuItem::Signal => 1,
+            MenuItem::Cheat => 2,
+            MenuItem::Trans => 3,
+        }
+    }
+}
+
 pub struct TabsComponent {}
 
 impl Default for TabsComponent {
@@ -9,6 +27,7 @@ impl Default for TabsComponent {
         TabsComponent {}
     }
 }
+
 impl TabsComponent {
     fn draw<B: Backend>(&self, f: &mut Frame<B>, area: Rect) {}
 }
