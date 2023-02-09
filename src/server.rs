ad545a308ac3e6575c1b015a003f34180d218c17
killer <michael.molchanov.2004@gmail.com>
2023-02-09 22:44:02 +0100

    server service address

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use crate::morser::messenger_server::MessengerServer;
 use crate::server::my_messenger_server::MyMessengerServer;
+use std::env;
 use std::net::ToSocketAddrs;
 use tonic::transport::Server;
 
@@ -7,10 +8,14 @@ mod my_messenger_server;
 mod utils;
 
 pub async fn execute() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args: Vec<String> = env::args().collect();
+    if args.is_empty() {
+        args.push("192.168.1.41:50051".to_string());
+    }
     let server = MyMessengerServer::default();
     Server::builder()
         .add_service(MessengerServer::new(server))
-        .serve("192.168.1.41:50051".to_socket_addrs().unwrap().next().unwrap())
+        .serve(args[0].to_socket_addrs().unwrap().next().unwrap())
         .await
         .unwrap();
     Ok(())
