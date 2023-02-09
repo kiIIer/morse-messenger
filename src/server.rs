07720b0122649899cf17f842ca32ef353dcb5b28
killer <michael.molchanov.2004@gmail.com>
2023-02-09 22:52:42 +0100

    fixed vector index

--- src/server.rs
+++ src/server.rs
@@ -9,13 +9,13 @@ mod utils;
 
 pub async fn execute() -> Result<(), Box<dyn std::error::Error>> {
     let mut args: Vec<String> = env::args().collect();
-    if args.is_empty() {
+    if args.len() < 2 {
         args.push("192.168.1.41:50051".to_string());
     }
     let server = MyMessengerServer::default();
     Server::builder()
         .add_service(MessengerServer::new(server))
-        .serve(args[0].to_socket_addrs().unwrap().next().unwrap())
+        .serve(args[1].to_socket_addrs().unwrap().next().unwrap())
         .await
         .unwrap();
     Ok(())
